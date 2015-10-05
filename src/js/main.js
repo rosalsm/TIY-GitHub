40d84939e48be427d4eb95f73ce53bde058438a0
rosa <r215130@gmail.com>
2015-10-05 19:05:17 -0400

    commiting in order to change branch

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,22 @@ jQuery.ajax('../apis/github/users/octocat.json').then(function(data)){
   console.log(data);
 
 });
+
+$('ul.tabs').each(function()){
+
+
+}
+
+
+
+
+
+$('a.contri').on('click', function(event)){
+
+}
+$('a.repos').on('click', function(event)){
+
+}
+$('a.publact').on('click', function(event)){
+
+}
